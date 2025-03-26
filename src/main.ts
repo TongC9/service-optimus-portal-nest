ae0e3d3a4f069987001dadc194557e2cd0977c4a
Tong <Natouch@idpassglobal.com>
2025-03-26 11:59:13 +0700

    swagger add

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,29 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { AppConfiguration } from './config/app.config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
+
+  const config = new DocumentBuilder()
+    .setTitle('Optimus Restful dashboard API')
+    .setDescription('The Optimus API document description')
+    .setVersion('1.0')
+    .addTag('Optimus API')
+    .addBearerAuth()
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config, {
+  // ignoreGlobalPrefix: false,
+});
+  SwaggerModule.setup('api', app, documentFactory, {
+  jsonDocumentUrl: 'swagger/json',
+});
+
+
   const appConfig = app.get(AppConfiguration);
   await app.listen(appConfig.appPort);
+  console.log(`Application is running on: ${await app.getUrl()}`);
+
 }
 bootstrap();
